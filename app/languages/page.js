af9770b9096abf328f4deeafcc3f4f555a6469f4
deepak0035 <kingdk0035@gmail.com>
2024-02-03 15:54:57 +0500

    bug fixed

--- app/languages/page.js
+++ app/languages/page.js
@@ -18,7 +18,7 @@ const page = () => {
       <LanguagesDropdown />
       <div className="flex justify-center items-center py-2">
         <button className="relative px-4 py-2 line-clamp-3 rounded-full bg-gradient-to-r from-carpetMoss to-carpetMoss via-green-500 text-white text-center h-12 w-4/5 md:h-16 md:w-96 font-semibold">
-          <span className="text-2xl">{title ? title : "Next"}</span>
+          <span className="text-2xl">{"Next"}</span>
           <span className="absolute top-3 right-4 md:top-5">
             <BsArrowRight className="text-2xl" />
           </span>
